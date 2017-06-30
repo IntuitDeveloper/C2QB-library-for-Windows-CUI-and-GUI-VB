5c12354d8a3287003da28db6f0edff51dd766512
nimisha shrivastava <nimisha_shrivastava@intuit.com>
2017-06-29 18:47:02 -0700

    Updated code to run backgroundworker

--- Test/FormC2QB.vb
+++ Test/FormC2QB.vb
@@ -1,23 +1,32 @@
-﻿
+
+Imports System.ComponentModel
 Imports System.Configuration
 
 Public Class FormC2QB
 
-        Private Sub C2QB_Click(sender As Object, e As EventArgs) Handles C2QB.Click
+    Private Sub C2QB_Click(sender As Object, e As EventArgs) Handles C2QB.Click
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object,
+                                         ByVal e As System.ComponentModel.DoWorkEventArgs) _
+                                         Handles BackgroundWorker1.DoWork
+        ' Do some time-consuming work on this thread.
+        System.Threading.Thread.Sleep(1000)
+
         Dim config As New QbConfig()
         config.ApplicationToken = ConfigurationSettings.AppSettings("applicationToken")
-            config.ConsumerKey = ConfigurationSettings.AppSettings("consumerKey")
-            config.ConsumerSecret = ConfigurationSettings.AppSettings("consumerSecret")
-            config.OauthRequestTokenEndpoint = ConfigurationSettings.AppSettings("oauthRequestTokenEndpoint")
-            config.OauthAccessTokenEndpoint = ConfigurationSettings.AppSettings("oauthAccessTokenEndpoint")
-            config.OauthBaseUrl = ConfigurationSettings.AppSettings("oauthBaseUrl")
-            config.OauthUserAuthUrl = ConfigurationSettings.AppSettings("oauthUserAuthUrl")
+        config.ConsumerKey = ConfigurationSettings.AppSettings("consumerKey")
+        config.ConsumerSecret = ConfigurationSettings.AppSettings("consumerSecret")
+        config.OauthRequestTokenEndpoint = ConfigurationSettings.AppSettings("oauthRequestTokenEndpoint")
+        config.OauthAccessTokenEndpoint = ConfigurationSettings.AppSettings("oauthAccessTokenEndpoint")
+        config.OauthBaseUrl = ConfigurationSettings.AppSettings("oauthBaseUrl")
+        config.OauthUserAuthUrl = ConfigurationSettings.AppSettings("oauthUserAuthUrl")
         config.SelectedMode = QbConfig.AppMode.Console
         Dim result As QbResponse = Nothing
         Dim connectToQuickBooks As New QbConnect()
         connectToQuickBooks.IConnect_Connect(config, result)
-        End Sub
+    End Sub
 
-    End Class
+End Class
 
 
