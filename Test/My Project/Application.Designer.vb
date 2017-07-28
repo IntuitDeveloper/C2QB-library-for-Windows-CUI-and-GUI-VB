70f52e63eb11b38a7715f14d87d47e5127c8e653
nimisha shrivastava <nimisha.shrivastava84@gmail.com>
2017-07-28 17:03:49 -0600

    Updated for  4.6.1

--- Test/My Project/Application.Designer.vb
+++ Test/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Test.FormC2QB
+            Me.MainForm = Global.Test.Form1
         End Sub
     End Class
 End Namespace
